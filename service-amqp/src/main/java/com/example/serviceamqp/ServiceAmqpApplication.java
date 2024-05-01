0c2425fd0b6ec1d12ab5d0235d3cb14ef472cb22
DenysSoldatenko <soldatenko911@gmail.com>
2024-05-01 10:00:00 +0300

    Added the service-amqp

---
+++ service-amqp/src/main/java/com/example/serviceamqp/ServiceAmqpApplication.java
@@ -0,0 +1,13 @@
+package com.example.serviceamqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAmqpApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ServiceAmqpApplication.class, args);
+  }
+
+}
