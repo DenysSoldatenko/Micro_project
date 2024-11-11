a72b6359f8b49fdcf0e0c9ac3238a2a06f2e15c9
DenysSoldatenko <soldatenko911@gmail.com>
2024-11-11 10:00:00 +0300

    Add missing Javadocs to server-eureka microservice

--- server-eureka/src/main/java/com/example/servereureka/ServerEurekaApplication.java
+++ server-eureka/src/main/java/com/example/servereureka/ServerEurekaApplication.java
@@ -4,12 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Entry point for the Eureka Server application.
+ * Enables the Eureka Server functionality to register and discover services.
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class ServerEurekaApplication {
 
+  /**
+   * Starts the Eureka Server application.
+   *
+   * @param args command line arguments
+   */
   public static void main(String[] args) {
     SpringApplication.run(ServerEurekaApplication.class, args);
   }
-
 }
+
