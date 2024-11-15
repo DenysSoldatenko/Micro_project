da9ff5541ec4ecfaf5c716cdf773f695e504ac3c
DenysSoldatenko <soldatenko911@gmail.com>
2024-11-15 10:00:00 +0300

    Add missing Javadocs to service-gateway microservice

--- service-gateway/src/main/java/com/example/servicegateway/ServiceGatewayApplication.java
+++ service-gateway/src/main/java/com/example/servicegateway/ServiceGatewayApplication.java
@@ -4,12 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * Main entry point for the Service Gateway application.
+ * Enables service discovery for the gateway.
+ */
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceGatewayApplication {
 
+  /**
+   * Main method to start the Service Gateway application.
+   *
+   * @param args command line arguments
+   */
   public static void main(String[] args) {
     SpringApplication.run(ServiceGatewayApplication.class, args);
   }
-
 }
